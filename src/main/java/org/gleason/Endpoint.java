6cfc7d03f2adec3fbebedd90c3567d13a3dd3c2b
Jackie Gleason <jackiegleason@gmail.com>
2018-04-12 14:36:43 -0400

    Adding the info to start working on JPA

--- src/main/java/org/gleason/Endpoint.java
+++ src/main/java/org/gleason/Endpoint.java
@@ -1,12 +1,19 @@
 package org.gleason;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class Endpoint{
+	@Autowired
+	AddressRepository repo;
 	@GetMapping("")
 	@ResponseBody
 	String get(){
-		return "Hello World";
+		String val = "";
+		for(AddressDomain domain: repo.findAll()){
+			val += domain.getCity();
+		}
+		return val;
 	}
 }
