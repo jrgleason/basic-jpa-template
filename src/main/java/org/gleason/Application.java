56e207b9b5dbb1b665d4985d5308d504d81749e2
Jackie Gleason <jackiegleason@gmail.com>
2018-04-24 14:58:25 -0400

    Basic Auth is working

--- src/main/java/org/gleason/Application.java
+++ src/main/java/org/gleason/Application.java
@@ -1,10 +1,16 @@
 package org.gleason;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:auth0.properties")
+})
 public class Application{
 	public static void main(String[] args){
 		SpringApplication.run(Application.class, args);
--- src/main/java/org/gleason/Endpoint.java
+++ src/main/java/org/gleason/Endpoint.java
@@ -1,12 +1,67 @@
 package org.gleason;
-import org.springframework.stereotype.Controller;
+import com.auth0.AuthenticationController;
+import com.auth0.IdentityVerificationException;
+import com.auth0.Tokens;
+import com.auth0.jwt.JWT;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
+
 @RestController
 public class Endpoint{
+	@Autowired
+	private AuthenticationController controller;
+	@Autowired
+	private AuthConfig appConfig;
+
 	@GetMapping("")
-	String get(){
-		return "hello world";
+	String get(final Principal principal){
+		if(principal instanceof TokenAuthentication){
+			return ((TokenAuthentication)principal).getUsername();
+		}
+		else{
+			return "Not logged in";
+		}
+	}
+
+	@GetMapping("/login")
+	protected void login(final HttpServletRequest req, final HttpServletResponse http) throws IOException{
+		String redirectUri = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + "/callback";
+		String authorizeUrl = controller.buildAuthorizeUrl(req, redirectUri)
+				.withScope("openid email")
+				.withAudience(String.format("https://%s/userinfo", appConfig.getDomain()))
+				.build();
+		http.sendRedirect(authorizeUrl);
+	}
+	@GetMapping("/callback")
+	protected String getCallback(final HttpServletRequest req, final HttpServletResponse res) throws IOException {
+        try {
+			Tokens tokens = controller.handle(req);
+			TokenAuthentication tokenAuth = new TokenAuthentication(JWT.decode(tokens.getIdToken()));
+			SecurityContextHolder.getContext().setAuthentication(tokenAuth);
+			res.sendRedirect("/");
+		} catch (IdentityVerificationException e) {
+			e.printStackTrace();
+			SecurityContextHolder.clearContext();
+			res.sendRedirect("/loginFailed");
+		}
+		return "Callback";
+	}
+	@GetMapping(value = "/logout2")
+	protected void logout(final HttpServletRequest req, final HttpServletResponse res) throws IOException{
+		invalidateSession(req);
+		res.sendRedirect("https://jackiergleason.auth0.com/v2/logout");
+	}
+	private void invalidateSession(HttpServletRequest request) {
+		if (request.getSession() != null) {
+			request.getSession().invalidate();
+		}
 	}
 }
